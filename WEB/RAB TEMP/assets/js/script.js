7e7f9642cc8d12c47d2c2bba9dc37c34e7da5c77
Sam <samuelkebbell@gmail.com>
2024-08-18 15:55:13 +0100

    updates

--- WEB/RAB TEMP/assets/js/script.js
+++ WEB/RAB TEMP/assets/js/script.js
@@ -1,33 +1,58 @@
-$( document ).ready(function() {
-                
-                $("#about_scroll").fadeOut();   
-                $("#work_scroll").fadeOut();
-                $("#contact_scroll").fadeOut();
+$(document).ready(function () {
+  $("#about_scroll").fadeOut();
+  $("#picks_scroll").fadeOut();
+  $("#music_scroll").fadeOut();
+  $("#games_scroll").fadeOut();
+  $("#totw_scroll").fadeOut();
+  $("#contact_scroll").fadeOut();
 
-                $("#about").click(function(){
-                    $("#index").fadeOut();
-                    $("#about_scroll").fadeIn();
-                    $('#about_left').addClass('animated slideInLeft');
-                    $('#about_right').addClass('animated slideInRight');
-                    });
-                $("#work").click(function(){
-                    $("#index").fadeOut();
-                    $("#work_scroll").fadeIn();
-                    $('#work_left').addClass('animated slideInLeft');
-                    $('#work_right').addClass('animated slideInRight');
-                    });
-                $("#contact").click(function(){
-                    $("#index").fadeOut();
-                    $("#contact_scroll").fadeIn();
-                    $('#contact_left').addClass('animated slideInLeft');
-                    $('#contact_right').addClass('animated slideInRight');
-                    });
-                
-                $(".back").click(function(){
-                    $(".pages").fadeOut();
-                    $("#index").fadeIn();
-                    $('#index_left').addClass('animated slideInLeft');
-                    $('#index_right').addClass('animated slideInRight');
-                    });
-           
-		});
+  $("#about").click(function () {
+    $("#index").fadeOut();
+    $("#about_scroll").fadeIn();
+    $("#about_left").addClass("animated slideInLeft");
+    $("#about_right").addClass("animated slideInRight");
+  });
+
+  $("#picks").click(function () {
+    $("#index").fadeOut();
+    $("#picks_scroll").fadeIn();
+    $("#picks_left").addClass("animated slideInLeft");
+    $("#picks_right").addClass("animated slideInRight");
+  });
+
+  $("#music").click(function () {
+    $("#index").fadeOut();
+    $("#music_scroll").fadeIn();
+    $("#music_left").addClass("animated slideInLeft");
+    $("#music_right").addClass("animated slideInRight");
+  });
+  $("#games").click(function () {
+    $("#index").fadeOut();
+    $("#games_scroll").fadeIn();
+    $("#games_left").addClass("animated slideInLeft");
+    $("#games_right").addClass("animated slideInRight");
+  });
+
+  $("#totw").click(function () {
+    $("#index").fadeOut();
+    $("#totw_scroll").fadeIn();
+    $("#totw_left").addClass("animated slideInLeft");
+    $("#totw_right").addClass("animated slideInRight");
+  });
+
+  $("#contact").click(function () {
+    $("#index").fadeOut();
+    $("#contact_scroll").fadeIn();
+    $("#contact_left").addClass("animated slideInLeft");
+    $("#contact_right").addClass("animated slideInRight");
+  });
+
+  $(".back").click(function () {
+    $(".pages").fadeOut();
+    $("#index").fadeIn();
+    $("#index_left").addClass("animated slideInLeft");
+    $("#index_right").addClass("animated slideInRight");
+  });
+});
+
+ 
