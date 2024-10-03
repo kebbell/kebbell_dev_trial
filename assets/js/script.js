23691068ebb862b8ca19970ac76873b70b17f970
Sam <samuelkebbell@gmail.com>
2024-10-03 16:19:27 +0100

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -597,3 +597,7 @@ let gameInterval = setInterval(gameLoop, 1000 / fps);
 // Add event listeners
 resetBtn.addEventListener('click', resetGame);
 backButton.addEventListener('click', goToHome);
+
+
+
+
